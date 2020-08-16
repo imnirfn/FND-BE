715845a484d0a33c4388edafa588ddd16550aa59
muaz <muazwazir@gmail.com>
2020-08-17 00:31:51 +0800

    error handling

--- src/controller/s3.controller.js
+++ src/controller/s3.controller.js
@@ -5,14 +5,12 @@ const s3 = connectToAWS();
 exports.getAllBucket = async (req, res) => {
   // Call S3 to list the buckets
   s3.listBuckets((err, data) => {
-    if (err) console.log(err);
+    if (err) res.status(404).json({ error: err });
 
-    console.log(data);
     res.json(data);
   });
 };
 
 exports.log = (req, res) => {
-  console.log(s3);
   res.json(s3);
 };
