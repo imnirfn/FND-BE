4973ca2bd275d0e71cd995016ddcca82f70cf024
Ubuntu <ubuntu@ip-172-31-42-207.ap-southeast-1.compute.internal>
2020-08-30 16:03:48 +0000

    add test prediction endpoint

--- src/api/test.js
+++ src/api/test.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const axios = require('axios');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json(['Test dev', '1', '2']);
+});
+
+router.post('/prediction', (req, res) => {
+	axios.post('http://127.0.0.1:5000', req.body)
+		.then(response => {
+			console.log(response.data)
+			return res.json(response.data)
+		})
+		.catch(err => console.log(err));
 });
 
 module.exports = router;
