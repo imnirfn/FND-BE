5ed998fca1cbd7c9300f809d929cbf49ea6227fe
muaz <muazwazir@gmail.com>
2020-08-31 21:28:44 +0800

    linting

--- src/api/test.js
+++ src/api/test.js
@@ -8,12 +8,12 @@ router.get('/', (req, res) => {
 });
 
 router.post('/prediction', (req, res) => {
-	axios.post('http://127.0.0.1:5000', req.body)
-		.then(response => {
-			console.log(response.data)
-			return res.json(response.data)
-		})
-		.catch(err => console.log(err));
+  axios.post('http://127.0.0.1:5000', req.body)
+    .then((response) => {
+      console.log(response.data);
+      return res.json(response.data);
+    })
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
