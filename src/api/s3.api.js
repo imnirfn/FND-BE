42f62561a22830a099a9d572d9a1ce411ca1b5d8
imnirfn <s3ns3xy@yahoo.com>
2020-09-01 12:12:48 +0800

    s3 upload

--- src/api/s3.api.js
+++ src/api/s3.api.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const s3Controller = require('../controller/s3.controller');
 
 router.get('/buckets', s3Controller.getAllBucket)
-  .get('/log', s3Controller.log);
+  .get('/log', s3Controller.log)
+  .post('/uploadDocument', s3Controller.uploadDocument);
 
 module.exports = router;
--- src/controller/s3.controller.js
+++ src/controller/s3.controller.js
@@ -2,6 +2,24 @@ const { connectToAWS } = require('../services/s3.services');
 
 const s3 = connectToAWS();
 
+exports.uploadDocument = async (req, res) => {
+  res.json(req.files);
+  // eslint-disable-next-line no-console
+  console.log(req.files.filename);
+
+  // try {
+  //   const uploadParams = { Bucket: 'tinggitecc', Key: req.files.name, Body: req.files.data };
+  //   await s3.upload(uploadParams, (err, data) => {
+  //     if (err) res.status(500).json({ error: err });
+  //     // eslint-disable-next-line no-console
+  //     console.log('Success');
+  //     res.json(data.Location);
+  //   }).promise();
+  // } catch (err) {
+  //   throw new Error(`S3 upload error: ${err.message}`);
+  // }
+};
+
 exports.getAllBucket = async (req, res) => {
   // Call S3 to list the buckets
   s3.listBuckets((err, data) => {
