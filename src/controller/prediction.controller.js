84a09b6d9801d317cf66c84255d528744048c1f4
imnirfn <s3ns3xy@yahoo.com>
2020-09-02 14:28:12 +0800

    added textract bucket

--- src/controller/prediction.controller.js
+++ src/controller/prediction.controller.js
@@ -1,12 +1,33 @@
 const { prediction } = require('../services/index.services');
 const { connectToLambda } = require('../services/index.services');
 
-// connect to lambda
 const lambda = connectToLambda();
+const { connectToAWS } = require('../services/s3.services');
+
+const s3 = connectToAWS();
 exports.triggerPrediction = prediction;
 
 exports.with_document = async (req, res) => {
+  res.json(req.files);
+  // eslint-disable-next-line no-console
+  console.log(req.files.filename);
 
+  try {
+    const uploadParams = {
+      Bucket: 'textractpipelinestack-documentsbucket9ec9deb9-6xorotgm13n8',
+      Key: req.files.filename.name,
+      Body: req.files.filename.data
+    };
+
+    await s3.upload(uploadParams, (err, data) => {
+      if (err) res.status(500).json({ error: err });
+      res.json(data.Location);
+    }).promise();
+  } catch (err) {
+    throw new Error(`S3 upload error: ${err.message}`);
+  }
+
+  // trigger lambda function to run prediction based on the S3
 };
 
 exports.with_text = async (req, res) => {
