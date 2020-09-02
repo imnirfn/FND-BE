ad21d460f3b37ad21926547f27d29efd45c84ec3
imnirfn <s3ns3xy@yahoo.com>
2020-09-02 13:55:08 +0800

    fix mistkes

--- src/api/prediction.api.js
+++ src/api/prediction.api.js
@@ -3,6 +3,7 @@ const predictionController = require('../controller/prediction.controller');
 
 router.post('/predict', predictionController.triggerPrediction)
   .post('/with_document', predictionController.with_document)
+  .post('/with_url', predictionController.with_url)
   .post('/with_text', predictionController.with_text);
 
 module.exports = router;
