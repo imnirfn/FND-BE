d38eb79a014941702255b51c6daa47d7bbc81ab5
muaz <muazwazir@gmail.com>
2020-09-04 02:43:47 +0800

    sagemaker

--- src/services/prediction.services.js
+++ src/services/prediction.services.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const BASE_URL = 'http://ec2-54-169-217-226.ap-southeast-1.compute.amazonaws.com:5000';
+const BASE_URL = 'http://localhost:5000';
 exports.callModelEndpoint = (param) => axios.post(BASE_URL, param)
   .then((res) => res.data)
   .catch((err) => {
