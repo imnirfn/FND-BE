4cbf2770e47bb3ca1bea4f78cdde3e498f33f765
muaz <muazwazir@gmail.com>
2020-09-04 03:43:24 +0800

    dynamo insert url

--- src/services/dynamoDB.services.js
+++ src/services/dynamoDB.services.js
@@ -6,7 +6,7 @@ exports.connectToDynamo = () => {
     const credentials = new AWS.SharedIniFileCredentials({ profile: 's3-bucket' });
     AWS.config.credentials = credentials;
 
-    AWS.config.update({ region: 'ap-southest-1' });
+    AWS.config.update({ region: 'ap-southeast-1' });
 
     dynamo = new AWS.DynamoDB.DocumentClient();
     if (!dynamo) throw new Error('Failed to connect to dynamo');
