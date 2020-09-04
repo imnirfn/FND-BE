19851ea7e1efe1ad02d81fcb12a145bd0dc0adbe
muaz <muazwazir@gmail.com>
2020-09-04 13:20:32 +0800

    asap babi

--- src/app.js
+++ src/app.js
@@ -27,6 +27,10 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.json({
